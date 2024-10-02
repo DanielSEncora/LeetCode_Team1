9e52585ed898e2e82577d302e5847faf6146e4c7
RaulUrbina <raul.urbina@encora.com>
2024-10-02 09:58:56 -0600

    Revert "Python Problems 1 - 3"
    This reverts commit b01d9ddbb8c21633b27e3e9609e63c566c615146.

--- Python/Problem1.py
+++
@@ -1,9 +0,0 @@
-#Median Of Two Sorted Array
-class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        arr = nums1 + nums2
-        mid = len(arr) // 2 
-        arr.sort()
-        if len(arr) % 2 == 0:
-            return (arr[mid] + arr[mid - 1]) / 2
-        return arr[mid]
